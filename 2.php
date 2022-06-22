b5e76c41584d9d84d73fcfb9d70b2e90c45d72e8
Građanin F <filipkr@protonmail.com>
2022-06-22 22:20:52 +0200

    Initial commit

---
+++ 2.php
@@ -0,0 +1,28 @@
+<!-- 
+Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
+Step 2: Add the two variables and display the sum with echo:
+Step3: Make 2 Arrays with the same values, one regular and the other associative 
+-->
+
+<?php
+
+$number1 = 10;
+$number2 = 20;
+$sum = $number1 + $number2;
+
+echo $sum;
+/* or */
+echo $number1 + $number2;
+
+$array1=['Filip', 'Tea', 'Mali', 'Grga'];
+$array2=[
+	'Filip' => 'Tea',
+	'Mali' => 'Grga'
+];
+
+echo '<pre>';
+print_r($array1);
+echo '<br/>';
+var_dump($array2);
+
+?>
---
+++ README.md
@@ -0,0 +1,4 @@
+## udemy-php
+### Practical exercises from an Udemy PHP course
+
+LINK: https://www.udemy.com/course/php-for-complete-beginners-includes-msql-object-oriented/
